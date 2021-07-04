6c8d09e47527b6a1dd59a646884e001e74dfd77d
pramodam21 <pramoda.m@robosoftin.com>
2021-07-04 12:33:22 +0530

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import Category from './Components/Category';
+import Products from './Components/Products';
 
 function App() {
   return (
     <div className="App">
-
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/category">Category</Link></li>
+          <li><Link to="/products">Products</Link></li>
+        </ul>
+      </nav>
+      <Route exact path="/"><Home /></Route>
+      <Route exact path="/category"><Category /></Route>
+      <Route exact path="/products"><Products /></Route>
     </div>
   );
 }
